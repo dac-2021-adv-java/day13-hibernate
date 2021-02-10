69b93d7fa94bf45d601a55a3913ce90a0b2724da
Santosh Mondal <santosh.ece06@gmail.com>
2021-02-10 11:14:01 +0530

    native query.

--- src/in/edac/entity/Employee.java
+++ src/in/edac/entity/Employee.java
@@ -4,13 +4,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "employee")
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String department;
+	private String project;
+	private String manager;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String department, String project, String manager) {
+		super();
+		this.department = department;
+		this.project = project;
+		this.manager = manager;
+	}
 
 	public int getId() {
 		return id;
@@ -26,6 +41,22 @@ public class Employee {
 
 	public void setDepartment(String department) {
 		this.department = department;
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public String getManager() {
+		return manager;
+	}
+
+	public void setManager(String manager) {
+		this.manager = manager;
 	}
 
 }
--- src/in/edac/entity/Student.java
+++ src/in/edac/entity/Student.java
@@ -4,13 +4,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "STUDENT")
 public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	private String email;
+	private String mobile;
+
+	public Student() {
+		super();
+	}
+
+	public Student(String name, String email, String mobile) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.mobile = mobile;
+	}
 
 	public int getId() {
 		return id;
@@ -26,6 +41,22 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
 	}
 
 }
